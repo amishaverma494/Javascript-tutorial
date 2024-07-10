365b3cfbe159881b37bb2d29c3cf4ab58ef64697
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-10 20:59:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//document.writen ('hello world');
+//alert ('hellonworld');
+//console.log('hello world);
+document.write('hello wo...optionalParams;any[]);
+    alert('hello people') void
+    console.log('hello world')
