1e2ff2ff5ddf7c150871e0695e9c36895a36f2d0
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-11 00:24:43 +0530

    Add files via upload

---
+++ variable(,let,const).js
@@ -0,0 +1,14 @@
+// LET vs CONST vs Var
+
+//using var
+var value=("some value");
+value=("some other value") ;
+//using let
+let name=("amisha") ;
+name=("prashant");
+//using const ( CONSTANT)-can't re-assign
+const lastName="smith";
+
+console.log(value);
+console.log(name);
+console.log(lastName);
---
+++ variable.js
@@ -0,0 +1,22 @@
+//variable- Most Basic Building Block
+//Variables-store ,access,modify==value
+//Declare ,Assingment operator,assign value
+
+let name=" amisha verma";
+let address,zip state;
+address"=ballia";
+zip="277202";
+state="up";
+name="amisha verma";
+    console.log('address,zip,state');
+    //some code
+    console.log('name');
+    //some code
+    console.log('name');
+    //some code
+    console.log('name');
+    //some code
+    console.log('name');
+    //some code
+    console.log('name');
+    //some code
