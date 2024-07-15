bb843d10d1da28edf25e10135c3b3f6013741686
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-15 14:05:20 +0530

    Add files via upload

---
+++ ARRAY.js
@@ -0,0 +1,17 @@
+// Arrays ,Functions and objects
+//Functions -declare ,invoke
+
+
+function hello() {
+    //logic 
+    console.log('HELLO THERE ITS AMISHA');
+    console.log('HELLO THERE ITS AMISHA');
+    console.log('HELLO THERE ITS AMISHA');
+    console.log('HELLO THERE ITS AMISHA');
+}
+hello();
+//COME  CODE  HERE....
+HELLO();
+//COME CODE HERE....
+hello();
+//COME CODE HERE....
---
+++ FUCTIONS.js
@@ -0,0 +1,18 @@
+// ARRAYS ,FUNCTIONS AND OBJECTS
+//ARRAYS -[],0 INDEX BASED
+
+const friend1 ='ANNA';
+const friend2='ANNA';
+const friend3 ='ANNA';
+const friend4='ANNA';
+
+const friendS['JOHN','PETER','BOB','SUSY',45,undefined, null];
+
+    let bestfriend=friendS[2];
+    friend4 [4]='ANNA';
+    console.log(friendS);
+    console.log(bestfriend);
+    console.log(friendS [4]);
+    
+    console.log(friendS[3]);
+    
