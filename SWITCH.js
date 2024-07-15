9d565d0420e1a2b30f6025caffb5e51b4da15fd2
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-16 00:46:47 +0530

    Add files via upload

---
+++ SWITCH.js
@@ -0,0 +1,15 @@
+//Switch
+//dice values :1-6
+
+const dice=1;
+
+if (dice ===1) {
+    console.log ('YOU GOT ONE');
+}
+if (dice===2){
+    console.log ('YOU GOT TWO');
+  
+}
+if (dice <1 || dice>6) {
+    console.log ('YOU GOT ONE');
+}
