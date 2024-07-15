b0020896e735b15b5b75684fc0603ccba899626c
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-15 23:54:49 +0530

    Add files via upload

---
+++ conditonal statements.js
@@ -0,0 +1,28 @@
+//conditional statements
+//Comparison Operators
+//>,<,>=,==,===,!=,!==
+//== checks only value
+//=== checks value and type
+
+const num1=8;
+const num2=8;
+const  result =num1>= num2;
+
+const value =false;
+
+if (! value) {
+    console.log ('value is false');
+}
+
+//if (num1>num2) {
+//console.log ('value is false');
+}
+
+//if (num1>num2){
+//console .log ('first number is bigger then second');
+//else if (result){
+//console .log ('first number is equal to second');
+//else if (result){
+//console .log ('second  number is bigger then first');
+//else if (result){
+//}
