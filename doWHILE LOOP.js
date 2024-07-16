f8e63ce04f2dac3cb2cb0c355b8520a031faa08b
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-17 00:01:02 +0530

    Add files via upload
    topics covered in this video is do while loop .for loop,connect two dots ,exercise -calculate total

---
+++ doWHILE LOOP.js
@@ -0,0 +1,13 @@
+//loops
+//repeatedly  run  a block of code  while condition is true 
+// do while loop 
+//Code block  first ,condition second
+//runs at least
+
+let money =0;
+
+do {
+    console.log ('you have '+ money+ ' dollars ');
+   money++ ;
+}
+while(money<10)
