ce3266b5036468ab4e626d999285c4b511a21795
Amisha Verma <137648331+amishaverma494@users.noreply.github.com>
2024-07-20 01:38:31 +0530

    Add files via upload

---
+++ global variabla.js
@@ -0,0 +1,12 @@
+//variable lookup
+//{} =code block
+
+
+const globalNumber=5;
+
+function add(num1,num2){
+    const result =num1+num2+ globalNumber;
+    return result;
+}
+    console.log (add(3,4));
+
